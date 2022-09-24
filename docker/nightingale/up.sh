7bc1a2d227e5fc2bbcd86493ba3f633294eefa3b
any <any@user.tax>
2022-09-24 22:41:51 +0800

    ♨

--- docker/nightingale/up.sh
+++ docker/nightingale/up.sh
@@ -4,5 +4,11 @@ DIR=$(dirname $(realpath "$0"))
 cd $DIR
 set -ex
 
+if [ ! -f "docker-compose.yml" ]; then
+cp docker-compose.init.yml docker-compose.yml
 docker-compose up -d
+fi
+
+sed -i -e "/^$1/c$2" docker-compose.yml
+
 ./nginx.sh

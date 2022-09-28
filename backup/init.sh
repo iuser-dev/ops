8fb711f1a1e1cb5b6489e38357ef64583891d2a0
any <any@user.tax>
2022-09-28 22:38:59 +0800

    ♨

---
+++ backup/init.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+DIR=$(dirname $(realpath "$0"))
+cd $DIR
+set -ex
+
+./cron.import.sh
+
+cd ../..
+
+git clone --depth=1 git@github.com:user-tax-dev/freeom_renew.git
+
+git clone --depth=1 --recursive git@github.com:user-tax-key/key.git
+
+cd key
+
+./init.sh

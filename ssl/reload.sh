12319c78184c192bbd99888b5f92a7ebb024e164
any <any@user.tax>
2022-09-28 19:40:30 +0800

    ♨

--- ssl/reload.sh
+++ ssl/reload.sh
@@ -4,10 +4,10 @@ DIR=$(dirname $(realpath "$0"))
 cd $DIR
 set -ex
 
-if ! [ -x "$(command -v nginx)" ]; then
+if [ -x "$(command -v nginx)" ]; then
 sudo service nginx reload
 fi
-if ! [ -x "$(command -v chasquid)" ]; then
+if [ -x "$(command -v chasquid)" ]; then
 sudo service chasquid restart
 fi
 ./run.sh
